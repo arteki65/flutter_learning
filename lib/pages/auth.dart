383f6ede7f7136cd78ca1d3af724cec13c631a1c
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2018-08-13 09:59:05 +0200

    after lecture 160

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:flutter_course/scoped-models/main_model.dart';
 
 class AuthPage extends StatefulWidget {
   @override
@@ -79,13 +81,13 @@ class _AuthPageState extends State<AuthPage> {
     );
   }
 
-  void _submitForm() {
+  void _submitForm(Function login) {
     if (!_formKey.currentState.validate() || !_formData['acceptTerms']) {
       return;
     }
 
     _formKey.currentState.save();
-    print(_formData);
+    login(_formData['email'], _formData['password']);
     Navigator.pushReplacementNamed(context, '/products');
   }
 
@@ -120,11 +122,19 @@ class _AuthPageState extends State<AuthPage> {
                     SizedBox(
                       height: 10.0,
                     ),
-                    RaisedButton(
-                      child: Text('LOGIN'),
-                      textColor: Colors.white,
-                      onPressed: _submitForm,
-                    ),
+                    ScopedModelDescendant<MainModel>(
+                      builder: (
+                        BuildContext context,
+                        Widget child,
+                        MainModel model,
+                      ) {
+                        return RaisedButton(
+                          child: Text('LOGIN'),
+                          textColor: Colors.white,
+                          onPressed: () => _submitForm(model.login),
+                        );
+                      },
+                    )
                   ],
                 ),
               ),
---
+++ lib/scoped-models/main_model.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_course/scoped-models/products_model.dart';
+import 'package:flutter_course/scoped-models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class MainModel extends Model with ProductsModel, UserModel {
+
+}
